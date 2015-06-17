e56bc494f0d0c20dffb31548ec519a72e2b48cc7
Eduardo Fonseca <ebf@Eduardos-MacBook-Pro.local>
2015-06-17 15:36:55 -0700

    Lets do this :)

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        less: {
+            development: {
+                options: {
+                    compress: false,
+                    cleancss: false,
+                    optimization: 2,
+                    dumpLineNumbers: 'false'
+                },
+                files: {
+                    "css/skeleton.css": "less/skeleton.less",
+                    "css/playlist.css": "less/playlist.less"
+                }
+            }
+        },
+
+        watch: {
+            options: {
+                livereload: false,
+            },
+            styles: {
+                files: ['less/**/*.less'], // which files to watch
+                tasks: ['less'],
+                options: {
+                    nospawn: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['less', 'watch']);
+};
---
+++ README.md
@@ -0,0 +1,19 @@
+# playlist-website
+
+## Getting started
+
+### Install Global Dependencies
+  * [Node.js](http://nodejs.org)
+  * [bower](http://bower.io): `[sudo] npm install bower -g`
+  * [grunt.js](http://grunt.js); `[sudo] npm install -g grunt-cli`
+
+### Install Local Dependencies
+  * clone the repo `git clone https://github.com/geniusme/playlist-website.git`
+  * cd to project folder `cd playlist-website`
+  * run `[sudo] npm install` (first time users)
+  * run `bower install`
+
+### Run Locally
+  * run `grunt` (to watch and compile Less files) creates a /css directory
+  * start a local server `python -m SimpleHTTPServer 8888` or serve from file path `file://`
+  * go to `localhost:8888` or `file://PATH-TO-PROJECT/playlist-website` to see the website
