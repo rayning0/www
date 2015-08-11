0f9145b9c0f78d14f2842300d607bfbc4fea0c47
Raymond Gan <rayning@yahoo.com>
2015-08-11 09:45:22 -0700

    Add authentication to upload.html page

--- jfu/js/main.js
+++ jfu/js/main.js
@@ -13,65 +13,52 @@
 
 $(function () {
     'use strict';
+    $('#buttons').hide();
+
+    $('#fileupload').submit(function() {
+      event.preventDefault();
+      $('#buttons').show();
+
+      // Load images already in database:
+      $('#fileupload').addClass('fileupload-processing');
+      $.ajax({
+          // Uncomment the following to send cross-domain cookies:
+          //xhrFields: {withCredentials: true},
+          url: $('#fileupload').fileupload('option', 'url'),
+          dataType: 'json',
+          context: $('#fileupload')[0],
+          beforeSend: function(xhr) {
+            xhr.setRequestHeader('Authorization', 'Basic ' +
+              btoa($('input:text').val() + ":" + $('input:password').val()));
+          }
+
+      }).always(function () {
+          $(this).removeClass('fileupload-processing');
+      }).done(function (result) {
+          $(this).fileupload('option', 'done')
+              .call(this, $.Event('done'), {result: result});
+      });
+    });
 
     // Initialize the jQuery File Upload widget:
     $('#fileupload').fileupload({
-        url: 'https://api.playlist.com/images', // production
-        // url: 'http://localhost:3000/images', // development
-        singleFileUploads: true,
-        // Uncomment the following to send cross-domain cookies:
-        //xhrFields: {withCredentials: true}
+      url: 'https://api.playlist.com/images', // production
+      // url: 'http://localhost:3000/images', // development
+      singleFileUploads: true,
+      // Uncomment the following to send cross-domain cookies:
+      // xhrFields: {withCredentials: true},
+      beforeSend: function(xhr) {
+        xhr.setRequestHeader('Authorization', 'Basic ' +
+          btoa($('input:text').val() + ":" + $('input:password').val()));
+      }
     });
 
-    // Enable iframe cross-domain access via redirect option:
-    $('#fileupload').fileupload(
-        'option',
-        'redirect',
-        window.location.href.replace(
-            /\/[^\/]*$/,
-            '/cors/result.html?%s'
-        )
-    );
-
-    if (window.location.hostname === 'blueimp.github.io') {
-        // Demo settings:
-        $('#fileupload').fileupload('option', {
-            url: '//jquery-file-upload.appspot.com/',
-            // Enable image resizing, except for Android and Opera,
-            // which actually support image resizing, but fail to
-            // send Blob objects via XHR requests:
-            disableImageResize: /Android(?!.*Chrome)|Opera/
-                .test(window.navigator.userAgent),
-            maxFileSize: 999000,
-            acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i
-        });
-        // Upload server status check for browsers with CORS support:
-        if ($.support.cors) {
-            $.ajax({
-                url: '//jquery-file-upload.appspot.com/',
-                type: 'HEAD'
-            }).fail(function () {
-                $('<div class="alert alert-danger"/>')
-                    .text('Upload server currently unavailable - ' +
-                            new Date())
-                    .appendTo('#fileupload');
-            });
+    $('#fileupload')
+      .bind('fileuploaddestroy', function (e, data) {
+        data.beforeSend = function(xhr) {
+          xhr.setRequestHeader('Authorization', 'Basic ' +
+          btoa($('input:text').val() + ":" + $('input:password').val()));
         }
-    } else {
-        // Load existing files:
-        $('#fileupload').addClass('fileupload-processing');
-        $.ajax({
-            // Uncomment the following to send cross-domain cookies:
-            //xhrFields: {withCredentials: true},
-            url: $('#fileupload').fileupload('option', 'url'),
-            dataType: 'json',
-            context: $('#fileupload')[0]
-        }).always(function () {
-            $(this).removeClass('fileupload-processing');
-        }).done(function (result) {
-            $(this).fileupload('option', 'done')
-                .call(this, $.Event('done'), {result: result});
-        });
-    }
+    });
 
 });
